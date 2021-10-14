9c0d93e80fa2bab5a6b6db5745bf622c44614b12
Dfam92 <dfam92@hotmail.com>
2021-10-14 14:33:29 -0300

    Starting GameDevPro
    Exercícios GameDevPro

---
+++ ExpertMode/Exercicio.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace ExpertMode
+{
+    public class Exercicio
+    {   static void Main(string[] args)
+        {   
+            int nota1 = int.Parse(Console.ReadLine());
+            int nota2 = int.Parse(Console.ReadLine());
+            int nota3 = int.Parse(Console.ReadLine());
+            int media = (nota1+nota2*2+nota3*3)/3;
+            Console.WriteLine(media);
+            Console.WriteLine($"Aluno aprovado?: {media>=60}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExpertMode/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExpertMode
+{
+    
+    public class Program
+    {
+        /*static void Main(string[] args)
+        {   
+            PrimeiroCadastro();
+            SegundoCadastro();
+            MensagemFinal();
+        }
+        public static string nome;
+        
+        static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void CadastrarNome()
+        {
+           nome = Console.ReadLine();
+        }
+        static void PrimeiroCadastro()
+        {
+            Print("Olá meu nome é professor Carvalho, Qual o seu nome? ");
+            CadastrarNome();
+        }
+
+        static void SegundoCadastro()
+        {
+            Print("Ok seu nome é : " + nome +"...");
+            Console.ReadKey();
+            Print("Agora precisamos saber o nome do meu neto...");
+            Console.ReadKey();
+            Print("Qual o nome do meu neto?");
+            CadastrarNome();
+        }
+        static void MensagemFinal()
+        {
+            Print("A verdade o nome dele é: " + nome + "...");
+            Console.ReadKey();
+            Print("Obrigado e vamos agora a nossa jornada!!!");
+            Console.ReadKey();
+        }*/
+
+       
+    }
+}
